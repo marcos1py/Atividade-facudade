f952aca07c733c4f8af0604b524fc746aa2ffd68
Marcos Antonio <marcosantoniop47@gmail.com>
2024-08-28 21:40:37 -0300

    arrumando o codigo para mandar ao professor

--- src/App.java
+++ src/App.java
@@ -10,26 +10,26 @@ public class App {
         for (int i = 0; i < vetor.length; i++) {
             if (vetor[i] < menorNumero) {
                 menorNumero = vetor[i];
-                // remove the extra closing brace
-                if (vetor[i] > MaiorNumero) {
-                    MaiorNumero = vetor[i];
-                }
+            }
+            if (vetor[i] > MaiorNumero) {
+                MaiorNumero = vetor[i];
             }
 
-            System.out.println("O menor número é: " + menorNumero);
-            System.out.println("O maior número é: " + MaiorNumero);
-
         }
-
-        int[][] matrizMae = { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+        System.out.println("O menor número é: " + menorNumero);
+        System.out.println("O maior número é: " + MaiorNumero);
+        int[][] matrizMae = { { 2, 2, 2 }, { 1, 1, 1 }, { 3, 21, 10 } };
 
         int valorTotal = 0;
         for (int i = 0; i < matrizMae.length; i++) {
+            valorTotal = 0;
             for (int j = 0; j < matrizMae[i].length; j++) {
                 valorTotal += matrizMae[i][j];
+
             }
+            System.out.println("linha " + i + ", valor total: " + valorTotal);
         }
-        System.out.println("valor total e " + valorTotal);
+
     }
 
 }
